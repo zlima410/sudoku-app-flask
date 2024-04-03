e90f6cbf8925034fc64c423ed8f1397d78e6acea
zlima410 <endercraft68@gmail.com>
2024-04-03 12:36:14 -0400

    Create /solve route and call it through the /get route to obtain info on the most recently generated puzzle

--- app.py
+++ app.py
@@ -65,5 +65,102 @@ def generate_puzzle():
         'id': new_puzzle.id  # Return the ID of the newly created puzzle
     })
 
+@app.route('/get', methods=['GET'])
+def get_last_puzzle():
+    # Query the database for the last puzzle based on the highest ID
+    last_puzzle = Sudoku.query.order_by(Sudoku.id.desc()).first()
+    
+    # Check if a puzzle was found
+    if last_puzzle:
+        return jsonify({
+            'status': 'success',
+            'message': 'Last puzzle retrieved successfully',
+            'id': last_puzzle.id,
+            'puzzle': last_puzzle.puzzle,
+            'solution': last_puzzle.solution  # Include this if you want to return the solution as well
+        })
+    else:
+        return jsonify({
+            'status': 'error',
+            'message': 'No puzzles found'
+        }), 404
+    
+def solve_sudoku(puzzle):
+    puzzle_list = [list(map(int, puzzle[i:i+9])) for i in range(0, 81, 9)]  # Convert string to list of lists
+    
+    def is_valid(num, pos, bo):
+        # Check row
+        for i in range(len(bo[0])):
+            if bo[pos[0]][i] == num and pos[1] != i:
+                return False
+
+        # Check column
+        for i in range(len(bo)):
+            if bo[i][pos[1]] == num and pos[0] != i:
+                return False
+
+        # Check box
+        box_x = pos[1] // 3
+        box_y = pos[0] // 3
+
+        for i in range(box_y*3, box_y*3 + 3):
+            for j in range(box_x*3, box_x*3 + 3):
+                if bo[i][j] == num and (i,j) != pos:
+                    return False
+
+        return True
+
+    def solve(bo):
+        find = find_empty(bo)
+        if not find:
+            return True
+        else:
+            row, col = find
+
+        for i in range(1,10):
+            if is_valid(i, (row, col), bo):
+                bo[row][col] = i
+
+                if solve(bo):
+                    return True
+
+                bo[row][col] = 0
+
+        return False
+
+    def find_empty(bo):
+        for i in range(len(bo)):
+            for j in range(len(bo[0])):
+                if bo[i][j] == 0:
+                    return (i, j)  # row, col
+
+    solve(puzzle_list)
+    return ''.join([''.join(str(num) for num in row) for row in puzzle_list])  # Convert list of lists back to string
+
+@app.route('/solve', methods=['GET'])
+def solve_last_puzzle():
+    # Get the last puzzle generated
+    last_puzzle = Sudoku.query.order_by(Sudoku.id.desc()).first()
+
+    if last_puzzle:
+        # Solve the puzzle
+        solution = solve_sudoku(last_puzzle.puzzle)
+
+        # Update the solution in the database
+        last_puzzle.solution = solution
+        db.session.commit()
+
+        return jsonify({
+            'status': 'success',
+            'message': 'Puzzle solved successfully',
+            'id': last_puzzle.id,
+            'solution': solution
+        })
+    else:
+        return jsonify({
+            'status': 'error',
+            'message': 'No puzzles found'
+        }), 404
+
 if __name__ == '__main__':
     app.run(debug=True)

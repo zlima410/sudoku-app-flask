107503cc63421f240e27c40dc6a17200954c1059
zlima410 <endercraft68@gmail.com>
2024-04-18 19:00:17 -0400

    Refactor SudokuGrid component to update puzzle values in updatePuzzle function

--- sudoku-front/src/components/SudokuGrid.js
+++ sudoku-front/src/components/SudokuGrid.js
@@ -1,30 +1,27 @@
-import React from 'react';
-import Cell from './Cell';
+import React from "react";
+import Cell from "./Cell";
 
 function SudokuGrid({ puzzle, setPuzzle }) {
-    const updatePuzzle = (newValue, row, col) => {
-        const newPuzzle = [...puzzle];
-        newPuzzle[row][col] = newValue;  // Update confirmed value
-        setPuzzle(newPuzzle);
-    };
+  const updatePuzzle = (newValue, row, col) => {
+    const newPuzzle = puzzle.map((r, i) => r.map((c, j) => (i === row && j === col ? newValue : c)));
+    setPuzzle(newPuzzle);
+  };
 
-    return (
-        <div className="grid">
-            {puzzle.map((row, rowIndex) =>
-                row.map((value, colIndex) => (
-                    <Cell
-                        key={`${rowIndex}-${colIndex}`}
-                        initialValue={value}
-                        updatePuzzle={updatePuzzle}
-                        rowIndex={rowIndex}
-                        colIndex={colIndex}
-                    />
-                ))
-            )}
-        </div>
-    );
+  return (
+    <div className="grid">
+      {puzzle.map((row, rowIndex) =>
+        row.map((cell, colIndex) => (
+          <Cell
+            key={`${rowIndex}-${colIndex}`}
+            initialValue={cell}
+            updatePuzzle={updatePuzzle}
+            rowIndex={rowIndex}
+            colIndex={colIndex}
+          />
+        ))
+      )}
+    </div>
+  );
 }
 
 export default SudokuGrid;
-
-
